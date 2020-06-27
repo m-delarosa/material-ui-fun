7f1b0c1078a18f2baad123af74b7358554682364
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-06-27 17:45:00 -0600

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Button } from '@material-ui/core'
+import { Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  helloThereStyle: {
+    fontStyle: 'oblique',
+    fontSize: "30px"
+  },
+  buttonStyles: {
+    color: 'green'
+  }
+})
 
 function App() {
+  const classes = useStyles()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography className={classes.helloThereStyle} color="secondary" variant="h1">Hello there.</Typography>
+      <Button className={classes.buttonStyles} variant="outlined" color="primary">THIS IS OUR FIRST BUTTON</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
